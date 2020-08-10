392212c7d0f252d7c44c28079a9381ad2b65ed5b
Gabi <gabrielbarrionuevo0@gmail.com>
2020-08-10 13:56:23 -0300

    Corrections on the script, the cp destination file was wrong. I've added the list of public_html

--- mksite.sh
+++ mksite.sh
@@ -3,7 +3,7 @@
 
 echo "Please type the name for the site"
 read
-mkdir -p mkdir -p /var/www/html/$REPLY/{public_html,logs}
-cp -R /var/www/html/src/wordpress/* /var/www/html/$REPLY/{public_html,logs}
+mkdir -p /var/www/html/$REPLY/{public_html,logs}
+cp -R /var/www/html/src/wordpress/* /var/www/html/$REPLY/public_html
 chown -R www-data:www-data /var/www/html/$REPLY
-ls -l /var/www/html/$REPLY
+ls -l /var/www/html/$REPLY/public_html
